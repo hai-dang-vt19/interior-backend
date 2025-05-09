d54315f16ab77a2b713731b3f613c6aa46367ccd
dangdh-bu2-fabbi <dangdh@fabbi.io>
2025-05-09 08:43:00 +0700

    Pending search with created at

--- app/Repositories/Customer/CustomerRepository.php
+++ app/Repositories/Customer/CustomerRepository.php
@@ -35,11 +35,12 @@ class CustomerRepository implements CustomerRepositoryInterface
                 }
             })
             ->when(isset($params['dateFrom']), function (Builder $query) use ($params) {
+                // chức năng này chưa hoạt động đúng
                 $explodeDate = explode(' - ', $params['dateFrom']);
                 if (count($explodeDate) == 1) {
                     // $startTime = Carbon::parse($explodeDate[0])->startOfDay();
                     // $endTime = Carbon::parse($explodeDate[0])->endOfDay();
-                    return $query->where('created_at', Carbon::parse($explodeDate[0])->toDateString());
+                    return $query->whereDate('created_at', Carbon::parse($explodeDate[0])->toDateString());
                 }
 
                 if (count($explodeDate) == 2) {
