37a12057508a27e2ef0d547533d67f674e20e841
dangdh <dangdh@fabbi.com.vn>
2026-03-21 21:36:15 +0700

    refactor: fix vite app scss

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ config('app.name', 'Laravel') }}</title>
         @vite([
-            'resources/css/app.css',
+            'resources/scss/app.scss',
             'resources/js/app.js',
             'resources/scss/custom.scss',
         ])
